450e4d38cc82a4b27e75043f170c82b3721c6b95
xiaobo lin <xiaobolin@xiaobos-MacBook-Pro.local>
2025-03-25 15:59:57 +0800

    Add changes

--- docker/development/php-fpm/entrypoint.sh
+++ docker/development/php-fpm/entrypoint.sh
@@ -11,9 +11,9 @@ chown -R ${USER_ID}:${GROUP_ID} /var/www/html || echo "Some files could not be c
 
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+#php artisan config:clear
+#php artisan route:clear
+#php artisan view:clear
 
 # Run the default command (e.g., php-fpm or bash)
 exec "$@"
